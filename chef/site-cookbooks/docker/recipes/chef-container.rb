ca70b75ed425d51c6b8f2d7d0eb899e5617f3bf1
imaizm@gmail.com <imaizm@gmail.com>
2014-12-11 22:55:42 +0900

    chef-containerのインストールレシピ追加

---
+++ chef/site-cookbooks/docker/recipes/chef-container.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: docker
+# Recipe:: chef-container
+#
+ruby_version = `source /etc/profile.d/rbenv.sh; rbenv global`.chop
+
+execute "rbenv exec gem install knife-solo" do
+	command "source /etc/profile.d/rbenv.sh; rbenv exec gem install --no-ri --no-rdoc knife-solo"
+	action :run
+	not_if { ::File.exists?("/usr/local/rbenv/versions/#{ruby_version}/bin/knife") }
+end
+
+execute "rbenv exec gem install berkshelf" do
+	command "source /etc/profile.d/rbenv.sh; rbenv exec gem install --no-ri --no-rdoc berkshelf"
+	action :run
+	not_if { ::File.exists?("/usr/local/rbenv/versions/#{ruby_version}/bin/berks") }
+end
+
+=begin
+execute "rbenv exec gem install knife-container" do
+	command "source /etc/profile.d/rbenv.sh; rbenv exec gem install --no-ri --no-rdoc knife-container"
+	action :run
+#	not_if { ::File.exists?("/usr/local/rbenv/versions/#{ruby_version}/bin/") }
+end
+=end
+
+execute "rbenv rehash" do
+	command "source /etc/profile.d/rbenv.sh; rbenv rehash"
+	action :run
+end
