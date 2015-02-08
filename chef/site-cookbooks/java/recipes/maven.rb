3cccb09d6b06f70a09bf611b0bac7eff3b6fc73a
imaizm <imaizm@gmail.com>
2015-02-09 02:38:47 +0900

    MAVENインストール手順完成

--- chef/site-cookbooks/java/recipes/maven.rb
+++ chef/site-cookbooks/java/recipes/maven.rb
@@ -1,7 +1,21 @@
 
-download_file = node["maven"]["download"]["file"]
-
-remote_file "#{Chef::Config[:file_cache_path]}/#{download_file}" do
+remote_file "#{Chef::Config[:file_cache_path]}/#{node['maven']['download']['file']}" do
 	source node["maven"]["download"]["url"]
 	action :create
+	not_if { File.exists?("/usr/local/#{node['maven']['download']['basename']}") }
+end
+
+bash "unarchive downloaded file" do
+	cwd "/usr/local"
+	code "tar xf #{Chef::Config[:file_cache_path]}/#{node['maven']['download']['file']}"
+	action :run
+	not_if { File.exists?("/usr/local/#{node['maven']['download']['basename']}") }
+end
+
+bash "Create /etc/profile.d/add_MAVEN_BIN_to_PATH.sh" do
+	code  <<-EOS
+		echo "PATH=\\$PATH:/usr/local/#{node['maven']['download']['basename']}/bin" > /etc/profile.d/add_MAVEN_BIN_to_PATH.sh
+	EOS
+	action :run
+	not_if { File.exists?("/etc/profile.d/add_MAVEN_BIN_to_PATH.sh") }
 end
