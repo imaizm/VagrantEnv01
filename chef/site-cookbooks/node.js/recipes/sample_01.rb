641a8750252b24be1cfeb4caed786529ca49e33f
imaizm@gmail.com <imaizm@gmail.com>
2015-06-03 02:35:16 +0900

    windowsでもnode installに失敗しないよう対応

--- chef/site-cookbooks/node.js/recipes/sample_01.rb
+++ chef/site-cookbooks/node.js/recipes/sample_01.rb
@@ -7,7 +7,7 @@ bash "npm install for sample_01" do
 		. /usr/local/nvm/nvm.sh
 		nvm use v#{node.version}
 		cd /var/www/src/node.js_sample_01
-		npm install
+		npm install --no-bin-links
 	EOH
 #	action :nothing
 #	subscribes :run, "bash[nvm.sh]", :delayed
