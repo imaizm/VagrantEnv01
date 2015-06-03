b7683a96988805642759e2f003f14341d7d671ea
imaizm <imaizm@gmail.com>
2015-06-03 09:55:55 +0900

    expressコマンドの仕様変更に対応

--- chef/site-cookbooks/node.js/recipes/express.rb
+++ chef/site-cookbooks/node.js/recipes/express.rb
@@ -6,7 +6,7 @@ bash "express" do
 	code <<-EOH
 		. /usr/local/nvm/nvm.sh
 		nvm use v#{node.version}
-		npm install -g express
+		npm install -g express-generator
 	EOH
 	action :nothing
 	subscribes :run, "bash[nvm.sh]", :delayed
