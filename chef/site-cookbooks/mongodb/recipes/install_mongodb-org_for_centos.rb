d670a33d0fd61b9996cbd2877f88bc78f0511d12
imaizm@gmail.com <imaizm@gmail.com>
2015-06-04 23:19:03 +0900

    mongodbインストールレシピ追加

---
+++ chef/site-cookbooks/mongodb/recipes/install_mongodb-org_for_centos.rb
@@ -0,0 +1,11 @@
+template "mongodb.repo" do
+	path "/etc/yum.repos.d/mongodb.repo"
+	owner "root"
+	group "root"
+	mode "0644"
+	source "mongodb.repo.erb"
+end
+
+package "mongodb-org" do
+	action :install
+end
