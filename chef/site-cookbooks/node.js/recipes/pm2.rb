2f9035e232ff517256479d89b44aeb5332ed7262
imaizm <imaizm@gmail.com>
2015-06-04 14:27:41 +0900

    pm2追加

---
+++ chef/site-cookbooks/node.js/recipes/pm2.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: node.js
+# Recipe:: pm2
+#
+bash "pm2" do
+	code <<-EOH
+		. /usr/local/nvm/nvm.sh
+		nvm use v#{node.version}
+		npm install -g pm2
+	EOH
+	action :nothing
+	subscribes :run, "bash[nvm.sh]", :delayed
+end
